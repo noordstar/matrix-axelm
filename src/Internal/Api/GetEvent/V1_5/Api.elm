e9b22e87f0953a3e980d35052004f726858abf5c
Bram van den Heuvel <bram.vdh1999@gmail.com>
2022-12-22 17:46:50 +0100

    Add v1.5 get event spec

--- src/Internal/Api/GetEvent/V1_5/Api.elm
+++ src/Internal/Api/GetEvent/V1_5/Api.elm
@@ -1,17 +1,17 @@
-module Internal.Api.GetEvent.V1_4.Api exposing (..)
+module Internal.Api.GetEvent.V1_5.Api exposing (..)
 
 import Internal.Api.GetEvent.Api as Api
-import Internal.Api.GetEvent.V1_3.Objects as PO
-import Internal.Api.GetEvent.V1_4.Convert as C
-import Internal.Api.GetEvent.V1_4.Objects as O
-import Internal.Api.GetEvent.V1_4.SpecObjects as SO
-import Internal.Api.GetEvent.V1_4.Upcast as U
+import Internal.Api.GetEvent.V1_4.Objects as PO
+import Internal.Api.GetEvent.V1_5.Convert as C
+import Internal.Api.GetEvent.V1_5.Objects as O
+import Internal.Api.GetEvent.V1_5.SpecObjects as SO
+import Internal.Api.GetEvent.V1_5.Upcast as U
 import Internal.Api.GetEvent.VersionControl as V
 
 
 packet : V.GetEventVersion Api.GetEventInputV1 PO.OldBlindEvent Api.GetEventInputV1 O.OldBlindEvent
 packet =
-    { version = "v1.4"
+    { version = "v1.5"
     , downcast = identity
     , current = Api.getEventInputV1 SO.clientEventDecoder C.convert
     , upcast = U.upcast
